1b9f57f2574669f12928dc9bc70f367ca271853c
sersangal <sersangal@gmail.com>
2021-02-27 14:25:03 +0100

    fix: trader syntax

--- bot/trader.py
+++ bot/trader.py
@@ -7,14 +7,14 @@ def trading(symbol: str, quantity: str):
     buy_oco_order = oco_order(symbol, "buy", buy_configuration, quantity)
     buy_oco_order_time = datetime.now()
     
-    buy_oco_result = oco_order_control(buy_oco_order, buy_oco_order_time):
+    buy_oco_result = oco_order_control(buy_oco_order, buy_oco_order_time)
 
 
     sell_configuration = get_sell_configuration(buy_oco_result)
     sell_oco_order = oco_order(symbol, "sell", sell_configuration, symbol_bought_quantity)
     sell_oco_order_time = datetime.now()
 
-    oco_order_control(sell_oco_order, sell_oco_order_time):
+    oco_order_control(sell_oco_order, sell_oco_order_time)
     
     if is_symbol_good_to_trading(symbol):
         quantity = get_bet_quantity()
@@ -24,14 +24,14 @@ def trading(symbol: str, quantity: str):
 
 
 def oco_order_control(oco_order: dict, oco_order_time: int) -> dict:
-    while:
+    while True:
         now = datetime.now()
         order_status = check_oco_status(oco_order)
         if order_status != "EXECUTING":
             order = get_order_from_order_list_id(oco_order["orderListId"])
             break
 
-        else if (now - oco_order_time) > bot_config.waiting_time_limit:
+        elif (now - oco_order_time) > bot_config.waiting_time_limit:
             cancel_oco_order(oco_order)
             break
     
@@ -42,6 +42,7 @@ def cancel_oco_order(oco_order: dict):
     # if oco_order is a buyer order, just cancel the order
     # if oco_order is a seller order, sell to the market price
     # TODO: call api endpoint
+    return
 
 
 def get_sell_configuration(order: dict) -> dict:
@@ -69,13 +70,13 @@ def oco_order(symbol: str, side: str, sell_configuration: dict, quantity: str) -
     # TODO: call api endpint
     return
 
-def get_current_price(order['symbol'])
+
+def get_current_price(symbol: str):
     # TODO: call api endpint
     return
 
 
-
-def get_candle(symbol: str, interval: str, number_of_intervals: int) -> list
+def get_candle(symbol: str, interval: str, number_of_intervals: int) -> list:
     return
 
 def is_symbol_good_to_trading(symbol: str) -> bool:
