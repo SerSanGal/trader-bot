bfac57f9d35c105469730314c1467c6519db59d2
sersangal <sersangal@gmail.com>
2021-02-28 13:32:48 +0100

    feat: main init

--- bot/main.py
+++ bot/main.py
@@ -1,53 +1,34 @@
+import binance_api
+import binance_config
+import calculations
+import filters
 import time
+import trader
 
-results = []
-# TODO: Get all open orders from Binance, in case that the program was previously closed.
-while True:
-  symbols_info = get_symbols_statistics_24hr() # List
-  balance = get_balance() # $100 in binance
-  risk_factor = 0.01 # % you are willing to bet 1% of $100 in balance 
-  bettable_coins = compose(
-    filterByPriceChange(0.3, '24 hours'),
-    discardNewbornCoins,
-    filterByPriceChange(0.2, '4 hours'),
-    filterByPriceChange(0.15, '1 hours'),
-  )
-  bet_quantity = calculate_bet_quantity(balance, risk_factor)  # ($100, 0.01) -> $1 is the bet quantity
-  bets_in_progress = get_bets_in_progress(results)
-  bettable_slots = max(calculate_number_of_bets(balance, bet_quantity, bets_in_progress)), len(bettable_coins)) # max((100 / 2) / 1), len(["BTC", "ETH", "DOGE"])) = 3 bettable slots
-  
-  # TODO: when you have more bettable coins than slots, pick the "best" coins. Best is ???
-  best_coins = pick_best_coins(bettable_coins, bettable_slots)
-  
-  # ~sort of bet function~
-  for slot, coin in zip(bettable_slots, best_coins):
-    buy_result = buy(coin, "market_price") # buys 1$ worth of [BTC, ETH...]
-    results[coin][order_id]["buyResult"] = buy_result
-    results[coin][order_id]["status"] = "progress"
+b_client = binance_api.BinanceAPI(binance_config.api_key, binance_config.api_secret)
 
-    profit = 0.02
-    price = bet_quantity * (1 + profit)
-    stop_price = bet_quantity
-    stop_limit_price = bet_quantity * (1 + (profit / 2))
-    timestamp = int(time.time * 1000)
-    sell_result = sell(coin, price, stop_price, stop_limit_price, order_id, timestamp)
-    results[coin][order_id]["sellResult"] = sell_result
+exchange_info = b_client.get_exchange_info()
+symbols = [symbol["symbol"] for symbol in exchange_info["symbols"]]
+filtered_symbols = filters.filter_symbols_by_buy_coin(symbols, "USDT")
 
-  for bet_in_progress in bets_in_progress:
-    order_list_id = bet_in_progress["orderListId"]
-    timestamp = int(time.time() * 1000)
-    bet_data = get_bet_data(order_list_id, timestamp)
-    bet_transaction_time = bet_data["transactionTime"]
-    bet_status = bet_data["listOrderStatus"]
-    symbol = bet_data["symbol"]
+print(len(filtered_symbols))
 
-    if bet_status == "FINISHED":
-      order_id = bet_data["listClientOrderId"]
-      results[symbol][order_id]["status"] = "finished"
-    else:
-      threshold_limit_in_milliseconds = 30 * 60 * 1000
-      bet_timestamp = bet_data["transactionTime"]
-      now = int(time.time() * 1000)
+quantity = 10 # USDT
 
-      if bet_timestamp + threshold_limit_in_milliseconds < now:
-        cancel_bet(symbol, order_list_id, now)
+# while true:
+
+bettable_symbols = []
+count = 0
+for symbol in filtered_symbols:
+    candles = b_client.get_klines_by_limit(symbol, "15m", 1)
+    is_candidate = calculations.is_bettable_symbol(candles[0])
+    print(symbol, is_candidate)
+    
+    if is_candidate:
+        bettable_symbols.append(symbol)
+        trader.trading(b_client, symbols, quantity, candles[0])
+    
+    time.sleep(1)
+
+print(bettable_symbols)
+
