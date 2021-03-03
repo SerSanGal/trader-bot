1c63e4a956a64edfb23192c5b8db86277b7983e2
sersangal <sersangal@gmail.com>
2021-03-03 13:17:15 +0100

    chore: formating

--- bot/filters.py
+++ bot/filters.py
@@ -1,22 +1,21 @@
-
-
 def filter_symbols_by_buy_coin(symbols: list, coin: str) -> list:
     return [symbol for symbol in symbols if symbol_match(symbol, coin)]
 
 
 def symbol_match(symbol: str, coin: str) -> bool:
-
-    sell_coin = symbol[0:len(symbol)-len(coin)]
+    sell_coin = symbol[0 : len(symbol) - len(coin)]
     if discard_special_coins(sell_coin):
         return False
-        
-    buy_coin = symbol[len(symbol)-len(coin):]
+
+    buy_coin = symbol[len(symbol) - len(coin) :]
     return buy_coin == coin
 
 
 def discard_special_coins(coin: str) -> bool:
     special_coins = ["UP", "DOWN"]
-    if any(special_coin for special_coin in special_coins if coin.find(special_coin)!=-1):
+    if any(
+        special_coin for special_coin in special_coins if coin.find(special_coin) != -1
+    ):
         return True
     else:
         return False
