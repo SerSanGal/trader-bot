c9e9e6ca43760a228ab7aa06c40de45e909920c8
sersangal <sersangal@gmail.com>
2021-03-05 12:31:53 +0100

    refactor: add the new bot config to buy function

--- bot/calculations.py
+++ bot/calculations.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 
 import bot_config
 
+
 def get_sweet_spot_to_buy(candle: list) -> dict:
     # open_time = candle[0] # in milliseconds
     # open_price = Decimal(candle[1]) 
@@ -11,13 +12,13 @@ def get_sweet_spot_to_buy(candle: list) -> dict:
     # close_time = candle[6] # in milliseconds
     # high_price = Decimal(candle[1]) 
 
-    price = close_price * ( 1 - bot_config.profit )
-    stop_limit_price = close_price * (1 + bot_config.tolerable_loss)
+    price = close_price * bot_config.buy_tolerance_top
+    stop_limit_price = close_price * bot_config.buy_tolerance_bottom
 
     return {
         "price": price_format(price),
         "stop_price": price_format(close_price),
-        "stop_limit_price": price_format(stop_limit_price)
+        "stop_limit_price": price_format(stop_limit_price),
     }
 
 def get_sweet_spot_to_sell(purchase_price: str, current_price: str) -> dict:
