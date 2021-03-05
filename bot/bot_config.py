3836150f2f0d2b8647303833e2717366c8d009e7
sersangal <sersangal@gmail.com>
2021-03-05 12:29:18 +0100

    feat: add new bot configuration variables

--- bot/bot_config.py
+++ bot/bot_config.py
@@ -2,7 +2,10 @@ from decimal import Decimal
 
 # bot configuration
 
-profit = Decimal(2 /100) # %
-tolerable_loss = Decimal(1 / 100) # %
+profit = Decimal(1 /100) # %
+tolerable_loss = Decimal(0.5 / 100) # %
 
-waiting_time_limit = 3 * 60 * 1000 # milliseconds
+buy_tolerance_top = Decimal(0.95)
+buy_tolerance_bottom = Decimal(1.05)
+
+waiting_time_limit = 1 * 60 * 1000 # milliseconds
