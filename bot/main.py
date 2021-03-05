916a7d51d25131a93a4e64ad196cc5a3abbffff2
sersangal <sersangal@gmail.com>
2021-03-05 12:36:22 +0100

    feat: infinity while to continuosly run the main

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,7 @@ import binance_config
 import calculations
 import filters
 import time
+from datetime import datetime
 import trader
 
 b_client = binance_api.BinanceAPI(binance_config.api_key, binance_config.api_secret)
@@ -15,20 +16,21 @@ print(len(filtered_symbols))
 
 quantity = 10 # USDT
 
-# while true:
+while True:
 
-bettable_symbols = []
-count = 0
-for symbol in filtered_symbols:
-    candles = b_client.get_klines_by_limit(symbol, "15m", 1)
-    is_candidate = calculations.is_bettable_symbol(candles[0])
-    print(symbol, is_candidate)
-    
-    if is_candidate:
-        bettable_symbols.append(symbol)
-        trader.trading(b_client, symbols, quantity, candles[0])
-    
-    time.sleep(1)
-
-print(bettable_symbols)
+    bettable_symbols = []
+    count = 0
+    for symbol in filtered_symbols:
+        candles = b_client.get_klines_by_limit(symbol, "1m", 5)
+        is_candidate = calculations.is_bettable_symbol(candles)
+        #print(symbol, is_candidate)
+        
+        if is_candidate:
+            bettable_symbols.append(symbol)
+            print(datetime.now(),symbol)
+            #trader.trading(b_client, symbols, quantity, candles[0])
+        
+        #time.sleep(1)
+    print("Round finished at: ", datetime.now())
+    #print(datetime.now(),bettable_symbols)
 
