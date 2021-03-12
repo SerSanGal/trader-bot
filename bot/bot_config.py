015dcab0716db90e18e5b3b4893114cc8f402888
sersangal <sersangal@gmail.com>
2021-03-12 10:27:13 +0100

    feat: add more bot config variables

--- bot/bot_config.py
+++ bot/bot_config.py
@@ -2,10 +2,12 @@ from decimal import Decimal
 
 # bot configuration
 
-profit = Decimal(1 /100) # %
-tolerable_loss = Decimal(0.5 / 100) # %
+profit = Decimal(2 / 100)  # %
+tolerable_loss = Decimal(0.5 / 100)  # %
 
-buy_tolerance_top = Decimal(0.95)
-buy_tolerance_bottom = Decimal(1.05)
+waiting_time_limit = 10  # seconds
+waiting_time_to_sell = 60 * 2  # seconds
 
-waiting_time_limit = 1 * 60 * 1000 # milliseconds
+bet = Decimal("20.00000000")
+
+coin = "USDT"
