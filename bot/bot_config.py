a3dc13391a32855501778bdde9f8729fa3361781
sersangal <sersangal@gmail.com>
2021-03-24 22:47:57 +0100

    feat: new bot config

--- bot/bot_config.py
+++ bot/bot_config.py
@@ -1,13 +1,5 @@
 from decimal import Decimal
 
 # bot configuration
-
-profit = Decimal(2 / 100)  # %
-tolerable_loss = Decimal(0.5 / 100)  # %
-
-waiting_time_limit = 10  # seconds
-waiting_time_to_sell = 60 * 2  # seconds
-
-bet = Decimal("20.00000000")
-
+profit = Decimal(1 / 100)  # %
 coin = "USDT"
