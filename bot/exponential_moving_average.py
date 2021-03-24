ad02a2934ba96f9e46c355b55d6575526cd98c0e
sersangal <sersangal@gmail.com>
2021-03-25 00:26:23 +0100

    fix: EMA calculation

--- bot/exponential_moving_average.py
+++ bot/exponential_moving_average.py
@@ -1,23 +1,23 @@
 from decimal import Decimal
 
 
-def calculate(candles: list, periode: int):
-    EMA = [0] * (periode - 1)
+def calculate(values: list, periode: int):
+    EMA = []
     k_prices = []
     k = Decimal(2 / (1 + periode))
     count = 0
-    for candle in candles:
+    for value in values:
         count += 1
-        k_price = Decimal(candle[4]) * k
+        k_price = Decimal(value) * k
         k_prices.append(k_price)
         if count == periode:
-            EMA.append(average(k_prices))
+            EMA.append(average(values[0:periode]))
         if count > periode:
             last_EMA = EMA[len(EMA) - 1] * (1 - k)
             EMA.append(last_EMA + k_price)
+    
     return EMA
 
 
 def average(data: list) -> Decimal:
     return Decimal(sum(data) / len(data))
-
