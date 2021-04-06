301885c79e7a5f50a848011ca4e451f85241e9b6
sersangal <sersangal@gmail.com>
2021-04-06 21:04:01 +0200

    feat: add new trader view function using talib lib

--- bot/tests/tests_trader_view.py
+++ bot/tests/tests_trader_view.py
@@ -1,7 +1,23 @@
 import pytest
 from decimal import Decimal
 
-from trader_view import get_sweet_spot_to_sell
+from trader_view import (
+    get_sweet_spot_to_sell,
+    bb_buy_signal,
+    bb_dump_signal,
+    rsi_buy_signal,
+    rsi_dump_signal,
+    storch_rsi_buy_signal,
+    macd_buy_signal,
+    storch_rsi_dump_signal,
+    get_closes_from_candles,
+    is_stable_coin,
+    is_moving_up,
+)
+import numpy
+import csv
+
+import pandas_datareader.data as web
 
 
 @pytest.mark.parametrize(
@@ -39,3 +55,130 @@ from trader_view import get_sweet_spot_to_sell
 def test_get_sweet_spot_to_sell(specs, expected_output):
     actual_output = get_sweet_spot_to_sell(specs)
     assert actual_output == expected_output
+
+
+def get_data(symbol):
+    data = []
+    with open("tests/data/" + symbol + ".csv", newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar="|")
+        next(reader, None)  # skip the headers
+        for row in reader:
+            data.append(numpy.double(row[1]))
+    return numpy.array(data)
+
+
+closes_1 = get_data("BTCUSDT")
+closes_2 = get_data("SUPERUSDT")
+closes_3 = get_data("FILUSDT")
+closes_4 = get_data("MTLUSDT")
+closes_5 = get_data("BUSDUSDT")
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, False,), (closes_2, True,)],
+)
+def test_bb_buy_signal(values, expected_output):
+    actual_output = bb_buy_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_3, True,), (closes_2, False,)],
+)
+def test_bb_dump_signal(values, expected_output):
+    actual_output = bb_dump_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, False,), (closes_4, True,)],
+)
+def test_rsi_buy_signal(values, expected_output):
+    actual_output = rsi_buy_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, False,), (closes_4, True,)],
+)
+def test_storch_rsi_buy_signal(values, expected_output):
+    actual_output = storch_rsi_buy_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, True,), (closes_4, False,)],
+)
+def test_macd_buy_signal(values, expected_output):
+    actual_output = macd_buy_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_3, True,), (closes_4, False,)],
+)
+def test_rsi_dump_signal(values, expected_output):
+    actual_output = rsi_dump_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, True,), (closes_4, False,)],
+)
+def test_storch_rsi_dump_signal(values, expected_output):
+    actual_output = storch_rsi_dump_signal(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_5, True,), (closes_4, False,)],
+)
+def test_is_stable_coin(values, expected_output):
+    actual_output = is_stable_coin(values)
+    assert actual_output == expected_output
+
+
+@pytest.mark.parametrize(
+    "values, expected_output", [(closes_1, True,), (closes_4, False,)],
+)
+def test_is_moving_up(values, expected_output):
+    actual_output = is_moving_up(values)
+    assert actual_output == expected_output
+
+
+def test_get_closes_from_candles():
+    candles = [
+        [
+            1499040000000,  # Open time
+            "0.01634790",  # Open
+            "0.80000000",  # High
+            "0.01575800",  # Low
+            "0.01577100",  # Close
+            "148976.11427815",  # Volume
+            1499644799999,  # Close time
+            "2434.19055334",  # Quote asset volume
+            308,  # Number of trades
+            "1756.87402397",  # Taker buy base asset volume
+            "28.46694368",  # Taker buy quote asset volume
+            "17928899.62484339",  # Ignore.
+        ],
+        [
+            1499040000000,  # Open time
+            "0.01634790",  # Open
+            "0.80000000",  # High
+            "0.01575800",  # Low
+            "0.01677100",  # Close
+            "148976.11427815",  # Volume
+            1499644799999,  # Close time
+            "2434.19055334",  # Quote asset volume
+            308,  # Number of trades
+            "1756.87402397",  # Taker buy base asset volume
+            "28.46694368",  # Taker buy quote asset volume
+            "17928899.62484339",  # Ignore.
+        ],
+    ]
+    expected_output = numpy.array(
+        [numpy.double("0.01577100"), numpy.double("0.01677100")]
+    )
+    actual_output = get_closes_from_candles(candles)
+    assert numpy.all(actual_output == expected_output)
--- bot/trader_view.py
+++ bot/trader_view.py
@@ -1,9 +1,11 @@
 import bot_config
 from decimal import Decimal
-from indicators import bollinger_band
 
 from binance import binance_config
 from formating import symbol_tick_size, price_format
+
+from talib import BBANDS, RSI, MA, STOCHRSI, MACD
+import numpy
 
 
 def get_sweet_spot_to_sell(specs: dict) -> dict:
@@ -24,23 +26,102 @@ def get_sweet_spot_to_sell(specs: dict) -> dict:
     return price_format(price, tick_size)
 
 
-def there_is_hype(candles: list) -> bool:
-    closes = [candle[4] for candle in candles]
-    last_close = closes[-1]
-    bb = bollinger_band.calculate(closes)
-
-    is_up = Decimal(last_close) > Decimal(bb["UBB"][-1])
-
-    is_volatile = (
-        Decimal(bb["UBB"]) / Decimal(bb["LBB"][-1]) - 1
-    ) > 5 * bot_config.profit
-
-    if is_up and is_volatile:
-        return is_bull_trend(candles)
+def is_a_buy_signal(closes):
+    if storch_rsi_buy_signal(closes) and bb_buy_signal(closes):
+        return True
     else:
         return False
 
 
+def is_moving_up(closes: list) -> bool:
+    ma_last = ma(closes)[-1]
+    return ma_last < closes[-1]
+
+
+def bb_buy_signal(closes: list) -> bool:
+    bbp_last = bbp(closes)[-1]
+    return bbp_last < 0
+
+
+def rsi_buy_signal(closes: list) -> bool:
+    rsi_last = rsi(closes)[-1]
+    return rsi_last < 30
+
+
+def storch_rsi_buy_signal(closes: list) -> bool:
+    storch_rsi_last = storch_rsi(closes)[-1]
+    return storch_rsi_last < 20
+
+
+def macd_buy_signal(closes: list) -> bool:
+    macd_hist_last = macd_hist(closes)[-1]
+    return macd_hist_last > 0
+
+
+def bb_dump_signal(closes: list) -> bool:
+    bbp_last = bbp(closes)[-1]
+    return bbp_last > 1
+
+
+def rsi_dump_signal(closes: list) -> bool:
+    rsi_last = rsi(closes)[-1]
+    return rsi_last > 70
+
+
+def storch_rsi_dump_signal(closes: list) -> bool:
+    storch_rsi_last = storch_rsi(closes)[-1]
+    return storch_rsi_last > 80
+
+
+def is_stable_coin(closes: list) -> bool:
+    stability = abs(((abs(numpy.std(closes)) + closes[-1]) / closes[-1]) - 1)
+    return stability < 0.005
+
+
+def ma(closes: list) -> list:
+    return MA(closes, timeperiod=60)
+
+
+def bb(closes: list) -> list:
+    return BBANDS(closes, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+
+
+def bbp(closes: list) -> list:
+    up, mid, low = bb(closes)
+
+    a = closes - low
+    b = up - low
+
+    bbp = numpy.divide(a, b, out=numpy.zeros_like(a), where=b != 0)
+    return bbp
+
+
+def rsi(closes: list) -> list:
+    return RSI(closes, timeperiod=14)
+
+
+def storch_rsi(closes: list) -> list:
+    fastk, fastd = STOCHRSI(
+        closes, timeperiod=14, fastk_period=5, fastd_period=3, fastd_matype=0
+    )
+    return fastk
+
+
+def macd_hist(closes: list) -> list:
+    macd, macdsignal, macdhist = MACD(
+        closes, fastperiod=12, slowperiod=26, signalperiod=9
+    )
+    return macdhist
+
+
+def get_closes_from_candles(candles: list) -> list:
+    closes = []
+    for candle in candles:
+        closes.append(numpy.double(candle[4]))
+    return numpy.array(closes)
+
+
+#  ----- OLD ------
 def is_bull_trend(candles: list, periode=60) -> bool:
     closes = [candle[4] for candle in candles]
     last_close = closes[-1]
@@ -51,8 +132,8 @@ def is_bull_trend(candles: list, periode=60) -> bool:
 def is_a_dip(candles: list) -> bool:
     closes = [candle[4] for candle in candles]
     last_close = closes[-1]
-    bb = bollinger_band.calculate(closes)
-    last_lower_bollinger_value = bb["LBB"][-1]
+    up, mid, low = BBANDS(closes, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    last_lower_bollinger_value = low[-1]
     if Decimal(last_close) < Decimal(last_lower_bollinger_value):
         return True
     else:
@@ -62,8 +143,8 @@ def is_a_dip(candles: list) -> bool:
 def is_a_buy_zone(candles: list) -> bool:
     closes = [candle[4] for candle in candles]
     last_close = closes[-1]
-    bb = bollinger_band.calculate(closes)
-    last_lower_bollinger_value = bb["LBB"][-1]
+    up, mid, low = BBANDS(closes, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+    last_lower_bollinger_value = low[-1]
     if Decimal(last_close) > Decimal(last_lower_bollinger_value):
         return True
     else:
